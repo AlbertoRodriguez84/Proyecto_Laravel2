a6846d0ad0d4a4b9176782ae3e1722fbbc81d330
Alberto Rodriguez <arodriguezpal@gmail.com>
2024-05-23 21:48:29 +0200

    Web con estilos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,19 +5,21 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Title</title>
+    @vite("resources/css/app.css")
 </head>
 <body>
-<header><h1 ">Esta es mi página principal</h1></header>
+<header><h1 class="h-15v bg-header">Esta es mi página principal</h1></header>
 <hr />
-<nav>
+<nav CLASS="h-10 bg-nav">
     Menu
 </nav>
 
-<main>
+<main class="h-65v bg-main">
     Parte Principal
 </main>
 
-<footer>
+<footer class="h-10v bg-footer">
     Footer
 </footer>
 </body>
+</html>
