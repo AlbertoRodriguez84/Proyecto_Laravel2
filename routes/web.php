7e610a30395e017f96f82e7b94e70ec9e0ec6b09
Alberto Rodriguez <arodriguezpal@gmail.com>
2024-05-23 21:38:57 +0200

    Web inicial funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\MainController;
 
-Route::get('/', function () {
+Route::get('/', [MainController::class, 'index'])->name('main');
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
